cdfcba484bc77ddcac4cf12c6ebbb97a05cd6f25
pruthvi-kotur <150178607+pruthvi-kotur@users.noreply.github.com>
2023-11-18 07:40:02 +0000

    added skills.html page

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app=Flask(__name__)
 @app.route('/')
 def home():
   return render_template("home.html")
+
+@app.route('/aboutme')
+def aboutme():
+  return render_template("aboutme.html")
+
+@app.route('/education')
+def education():
+  return render_template("education.html")
+
+@app.route('/skills')
+def skills():
+  return render_template("skills.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',debug=True)
